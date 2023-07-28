686ca2347817ee913d8e53f51846a4d988cdb899
莘权 马 <mashenquan@fuzhi.ai>
2023-07-28 15:33:52 +0800

    fixbug: startup parameters do not match

--- startup.py
+++ startup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-@Modified By: mashenquan, 2023-07-27, +industry concept
+@Modified By: mashenquan, 2023-07-27, + `industry` concept
 """
 
 import asyncio
@@ -15,9 +15,9 @@ from metagpt.roles.teacher import Teacher
 from metagpt.software_company import SoftwareCompany
 
 
-async def software_startup(investment: float = 3.0, n_round: int = 5, code_review: bool = False, *args, **kwargs):
+async def software_startup(idea: str, investment: float = 3.0, n_round: int = 5, *args, **kwargs):
     """Run a startup. Be a boss in software industry."""
-    idea = kwargs['idea']  # Your innovative idea, such as "Creating a snake game."
+    code_review = kwargs.get("code_review", False)  # Whether to use code review.
     company = SoftwareCompany()
     company.hire([ProductManager(),
                   Architect(),
@@ -28,7 +28,7 @@ async def software_startup(investment: float = 3.0, n_round: int = 5, code_revie
     await company.run(n_round=n_round)
 
 
-async def education_startup(investment: float = 3.0, n_round: int = 5, code_review: bool = False, *args, **kwargs):
+async def education_startup(lesson_file: str, investment: float = 3.0, n_round: int = 1, *args, **kwargs):
     """Run a startup. Be a teacher in education industry."""
 
     demo_lesson = """
@@ -76,7 +76,6 @@ async def education_startup(investment: float = 3.0, n_round: int = 5, code_revi
     """
 
     lesson = ""
-    lesson_file = kwargs.get('lesson_file')
     if lesson_file is not None and Path(lesson_file).exists():
         async with aiofiles.open(lesson_file, mode="r", encoding="utf-8") as reader:
             lesson = await reader.read()
@@ -92,12 +91,12 @@ async def education_startup(investment: float = 3.0, n_round: int = 5, code_revi
     await company.run(n_round=1)
 
 
-def main(investment: float = 3.0, n_round: int = 5, code_review: bool = False, *args, **kwargs):
+def main(idea: str, investment: float = 3.0, n_round: int = 5, *args, **kwargs):
     """
     We are a software startup comprised of AI. By investing in us, you are empowering a future filled with limitless possibilities.
+    :param idea: Your innovative idea for `software` industry, such as "Creating a snake game."; lesson filename for `education` industry.
     :param investment: As an investor, you have the opportunity to contribute a certain dollar amount to this AI company.
     :param n_round:
-    :param code_review: Whether to use code review.
     :param args: Parameters passed in format: `python your_script.py arg1 arg2 arg3`
     :param kwargs: Parameters passed in format: `python your_script.py a--param1=value1 --param2=value2`
     :return:
@@ -111,7 +110,7 @@ def main(investment: float = 3.0, n_round: int = 5, code_review: bool = False, *
     if startup is None:
         print(f"Available industries:{list(industries.keys())}")
         return
-    asyncio.run(startup(investment, n_round, code_review, *args, **kwargs))
+    asyncio.run(startup(idea, investment, n_round, *args, **kwargs))
 
 
 if __name__ == '__main__':
