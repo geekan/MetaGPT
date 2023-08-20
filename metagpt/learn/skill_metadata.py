c41f16e7bc58a3df13f04cdf000f4d41c580df76
莘权 马 <mashenquan@fuzhi.ai>
2023-08-20 19:24:10 +0800

    feat: +skill meta data decorator

--- metagpt/learn/skill_metadata.py
+++ metagpt/learn/skill_metadata.py
@@ -7,11 +7,11 @@
 @Desc    : Defines metadata for the `skill`.
         Depending on the context and specific circumstances, skills may have different effects.
         For example:
-            Proprietor: "Skill of the proprietor entity."（所有者的技能）
-            Holder: "Skill of the holder entity."（持有者的技能）
-            Possessor: "Skill of the possessor entity."（拥有者的技能）
-            Controller: "Skill of the controller entity."（控制者的技能）
-            Owner: "Skill of the owner entity."（所有者的技能）
+            Proprietor: "Skill of the proprietor entity."
+            Holder: "Skill of the holder entity."
+            Possessor: "Skill of the possessor entity."
+            Controller: "Skill of the controller entity."
+            Owner: "Skill of the owner entity."
 """
 
 
