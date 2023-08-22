5121472bd85e9cac565cc08bf5c763a00de522fb
莘权 马 <mashenquan@fuzhi.ai>
2023-08-22 10:23:42 +0800

    feat: +Message to __init__

--- metagpt/__init__.py
+++ metagpt/__init__.py
@@ -3,6 +3,7 @@
 # @Time    : 2023/4/24 22:26
 # @Author  : alexanderwu
 # @File    : __init__.py
+# @Desc    : mashenquan, 2023/8/22. Add `Message` for importing by external projects.
 
 from metagpt.schema import Message
 
