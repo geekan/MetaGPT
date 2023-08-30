9428c256caf1f16971216c7a3e4b66603bf8a825
莘权 马 <mashenquan@fuzhi.ai>
2023-08-30 17:55:13 +0800

    feat: +metagpt llm

---
+++ metagpt/provider/metagpt_llm_api.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2023/8/30
+@Author  : mashenquan
+@File    : metagpt_llm_api.py
+@Desc    : MetaGPT LLM related APIs
+"""
+
+import openai
+
+from metagpt.config import CONFIG
+from metagpt.provider import OpenAIGPTAPI
+from metagpt.provider.openai_api import RateLimiter
+
+
+class MetaGPTLLMAPI(OpenAIGPTAPI):
+    """MetaGPT LLM api"""
+
+    def __init__(self):
+        self.__init_openai(CONFIG)
+        self.llm = openai
+        self.model = CONFIG.METAGPT_API_MODEL
+        self.auto_max_tokens = False
+        RateLimiter.__init__(self, rpm=self.rpm)
+
+    def __init_openai(self, config):
+        openai.api_key = CONFIG.METAGPT_API_KEY
+        if config.openai_api_base:
+            openai.api_base = CONFIG.METAGPT_API_BASE
+        if config.openai_api_type:
+            openai.api_type = CONFIG.METAGPT_API_TYPE
+            openai.api_version = CONFIG.METAGPT_API_VERSION
+        self.rpm = int(config.get("RPM", 10))
