96f833cf8fafcea3555efd5871bea2ed2364647f
莘权 马 <mashenquan@fuzhi.ai>
2023-09-04 12:47:45 +0800

    feat: +redis

--- metagpt/memory/brain_memory.py
+++ metagpt/memory/brain_memory.py
@@ -6,7 +6,7 @@
 @File    : brain_memory.py
 @Desc    : Support memory for multiple tasks and multiple mainlines.
 """
-
+import hashlib
 from enum import Enum
 from typing import Dict, List
 
@@ -28,6 +28,10 @@ class BrainMemory(pydantic.BaseModel):
     stack: List[Dict] = []
     solution: List[Dict] = []
     knowledge: List[Dict] = []
+    # If the fingerprint of the history text is found in the `historical_summary_fingerprint`,
+    # it indicates that the text has already been incorporated into the `history summary`.
+    historical_summary_fingerprint: List[str] = []
+    historical_summary: str = ""
 
     def add_talk(self, msg: Message):
         msg.add_tag(MessageType.Talk.value)
@@ -58,17 +62,19 @@ class BrainMemory(pydantic.BaseModel):
         return "\n".join(texts)
 
     def move_to_solution(self, history_summary):
-        """放入solution队列，以备后续长程检索。目前还未加此功能，先用history_summary顶替"""
-        if len(self.history) < 2:
-            return
-        msgs = self.history[:-1]
-        self.solution.extend(msgs)
-        if not Message(**self.history[-1]).is_contain(MessageType.Talk.value):
-            self.solution.append(self.history[-1])
-            self.history = []
-        else:
-            self.history = self.history[-1:]
-        self.history.insert(0, Message(content="RESOLVED: " + history_summary))
+        """Put it in the solution queue for future long-term retrieval.
+        This functionality hasn't been added yet, so use the history summary as a temporary substitute for now."""
+        pass
+        # if len(self.history) < 2:
+        #     return
+        # msgs = self.history[:-1]
+        # self.solution.extend(msgs)
+        # if not Message(**self.history[-1]).is_contain(MessageType.Talk.value):
+        #     self.solution.append(self.history[-1])
+        #     self.history = []
+        # else:
+        #     self.history = self.history[-1:]
+        # self.history.insert(0, Message(content="RESOLVED: " + history_summary))
 
     @property
     def last_talk(self):
@@ -78,3 +84,7 @@ class BrainMemory(pydantic.BaseModel):
         if not last_msg.is_contain(MessageType.Talk.value):
             return None
         return last_msg.content
+
+    @staticmethod
+    def get_md5(text: str) -> str:
+        return hashlib.md5(text.encode()).hexdigest()
