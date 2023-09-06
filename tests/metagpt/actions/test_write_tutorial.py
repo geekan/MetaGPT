473baf193c4dd8f689e3d9295b0ca8731cd2df3a
Stitch-z <284618289@qq.com>
2023-09-06 23:38:46 +0800

    update: add unit test for the role tutorial assistant

---
+++ tests/metagpt/actions/test_write_tutorial.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+"""
+@Time    : 2023/9/6 21:41:34
+@Author  : Stitch-z
+@File    : test_write_tutorial.py
+"""
+from typing import Dict
+
+import aiofiles
+import pytest
+
+from metagpt.actions.write_tutorial import WriteDirectory, WriteContent, SaveDocx
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    ("language", "topic"),
+    [("English", "Write a tutorial about Python")]
+)
+async def test_write_directory(language: str, topic: str):
+    ret = await WriteDirectory(language=language).run(topic=topic)
+    assert isinstance(ret, dict)
+    assert "title" in ret
+    assert "directory" in ret
+    assert isinstance(ret["directory"], list)
+    assert len(ret["directory"])
+    assert isinstance(ret["directory"][0], dict)
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    ("language", "topic", "directory"),
+    [("English", "Write a tutorial about Python", {"Introduction": ["What is Python?", "Why learn Python?"]})]
+)
+async def test_write_content(language: str, topic: str, directory: Dict):
+    ret = await WriteContent(language=language, directory=directory).run(topic=topic)
+    assert isinstance(ret, str)
+    assert list(directory.keys())[0] in ret
+    for value in list(directory.values())[0]:
+        assert value in ret
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    ("title", "content"),
+    [("Python", "Write a tutorial about Python")]
+)
+async def test_save_docx(title: str, content: str):
+    ret = await SaveDocx().run(title=title, content=content)
+    assert isinstance(ret, str)
+    assert title in ret
+    async with aiofiles.open(ret, mode="r") as reader:
+        body = await reader.read()
+        assert body == content
