832294809b097793dff3472b1183aed37f8f5c8d
莘权 马 <mashenquan@fuzhi.ai>
2023-09-07 11:01:27 +0800

    feat: + LLMType

--- metagpt/llm.py
+++ metagpt/llm.py
@@ -4,10 +4,30 @@
 @Time    : 2023/5/11 14:45
 @Author  : alexanderwu
 @File    : llm.py
+@Modified By: mashenquan, 2023
 """
+from enum import Enum
 
 from metagpt.provider.anthropic_api import Claude2 as Claude
 from metagpt.provider.openai_api import OpenAIGPTAPI as LLM
+
+
+class LLMType(Enum):
+    OPENAI = "OpenAI"
+    METAGPT = "MetaGPT"
+    UNKNOWN = "UNKNOWN"
+
+    @classmethod
+    def get(cls, value):
+        for member in cls:
+            if member.value == value:
+                return member
+        return cls.UNKNOWN
+
+    @property
+    def UNKNOWN(self):
+        return LLMType.UNKNOWN
+
 
 DEFAULT_LLM = LLM()
 CLAUDE_LLM = Claude()
@@ -15,6 +35,6 @@ CLAUDE_LLM = Claude()
 
 async def ai_func(prompt):
     """使用LLM进行QA
-       QA with LLMs
-     """
+    QA with LLMs
+    """
     return await DEFAULT_LLM.aask(prompt)
