098027d249e709c4a939d8feb042e76f26ef0116
莘权 马 <mashenquan@fuzhi.ai>
2023-09-07 14:23:36 +0800

    feat: + summary

--- metagpt/provider/metagpt_llm_api.py
+++ metagpt/provider/metagpt_llm_api.py
@@ -7,24 +7,17 @@
 """
 import json
 
-import openai
 from pydantic import BaseModel
 
-from metagpt.config import CONFIG
 from metagpt.memory.brain_memory import BrainMemory
 from metagpt.provider import OpenAIGPTAPI
-from metagpt.provider.openai_api import RateLimiter
 
 
 class MetaGPTLLMAPI(OpenAIGPTAPI):
     """MetaGPT LLM api"""
 
     def __init__(self):
-        self.llm = openai
-        self.model = CONFIG.METAGPT_API_MODEL
-        self.auto_max_tokens = False
-        self.rpm = int(CONFIG.RPM) if CONFIG.RPM else 10
-        RateLimiter.__init__(self, rpm=self.rpm)
+        super().__init__()
 
     async def get_summary(self, memory: BrainMemory, max_words=200, keep_language: bool = False, **kwargs):
         summary = []
