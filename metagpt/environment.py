9695e8978367e6772ea6962c6e7985ba0eb1362d
洪沐天 <1747638932@qq.com>
2023-09-19 02:05:16 +0000

    开始开发中文版

--- metagpt/environment.py
+++ metagpt/environment.py
@@ -14,6 +14,7 @@ from metagpt.memory import Memory
 from metagpt.roles import Role
 from metagpt.schema import Message
 
+
 class Environment(BaseModel):
     """环境，承载一批角色，角色可以向环境发布消息，可以被其他角色观察到
        Environment, hosting a batch of roles, roles can publish messages to the environment, and can be observed by other roles
