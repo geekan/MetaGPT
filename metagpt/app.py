c1a7978c24227589439f1b023b6f6bea425c4321
洪沐天 <1747638932@qq.com>
2023-09-20 06:12:13 +0000

    Attempt to comply with PEP8 standards

--- metagpt/app.py
+++ metagpt/app.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import aiofiles
+"""
+@Time    : 2023/9/19 17:12
+@Author  : ORI_Replication
+@File    : app.py
+"""
 import gradio as gr 
 from metagpt.software_company import SoftwareCompany, SoftwareCompanyWithHuman
-from metagpt.roles import ProjectManager, ProductManager, Architect, Engineer, QaEngineer, Searcher, Sales, customer_service
+from metagpt.roles import ProjectManager,\
+                ProductManager, Architect,\
+                Engineer, QaEngineer
 from metagpt.roles.researcher import RESEARCH_PATH, Researcher
-import io
-import asyncio
-import threading
 from metagpt.const import PROJECT_ROOT
-from time import sleep
-from metagpt.schema import Message
 import sys
 
 def clear_logs():
@@ -58,7 +58,12 @@ async def startup(company_name : str,
         await company.run(n_round)
     elif company_name == "SoftwareCompany_With_Human":
         await company.continue_run()
-    return "Role: "+company.environment.short_term_history.role, "Action: "+str(company.environment.short_term_history.cause_by),company.environment.short_term_history.sent_from,company.environment.short_term_history.send_to, company.environment.short_term_history.content, company.environment.short_term_history.content
+    return  "Role: "+company.environment.short_term_history.role,\
+            "Action: "+str(company.environment.short_term_history.cause_by),\
+            company.environment.short_term_history.sent_from,\
+            company.environment.short_term_history.send_to,\
+            company.environment.short_term_history.content,\
+            company.environment.short_term_history.content
 
 async def __continue(message_content : str):
     company = SoftwareCompany_Company
@@ -67,7 +72,11 @@ async def __continue(message_content : str):
     company.environment.memory.add(company.environment.short_term_history)
     company.environment.history += f"\n{company.environment.short_term_history}"
     await company.continue_run()
-    return "Role: "+company.environment.short_term_history.role, "Action: "+str(company.environment.short_term_history.cause_by),company.environment.short_term_history.sent_from,company.environment.short_term_history.send_to, company.environment.short_term_history.content, company.environment.short_term_history.content
+    return      "Role: "+company.environment.short_term_history.role,\
+                "Action: "+str(company.environment.short_term_history.cause_by),\
+                company.environment.short_term_history.sent_from,company.environment.short_term_history.send_to,\
+                company.environment.short_term_history.content,\
+                company.environment.short_term_history.content
 
 async def research_startup(language : str,
                            topic : str):
@@ -92,13 +101,18 @@ with app:
                 """)
     with gr.Tabs():
         with gr.TabItem("MetaGPT") as generate_tab:
-            company_choise = gr.Dropdown(label = "Choose the company type", choices = ["SoftwareCompany", "SoftwareCompany_With_Human"], value = "SoftwareCompany_With_Human")
+            company_choise = gr.Dropdown(label = "Choose the company type",\
+                                        choices = ["SoftwareCompany", "SoftwareCompany_With_Human"],\
+                                        value = "SoftwareCompany_With_Human")
             with gr.Row():
-                investment = gr.Slider(minimum=0.0, maximum=20.0, step=0.1, label="Investment",value = 6.0, info="The maxmium money you want to spend on the GPT generation")
+                investment = gr.Slider(minimum=0.0, maximum=20.0, step=0.1, label="Investment",value = 6.0,\
+                                        info="The maxmium money you want to spend on the GPT generation")
                 n_round = gr.Number( label="Round", value = 5, info="The maxmium round you want to run",visible = False)
             with gr.Row():
                 code_review = gr.Checkbox(label = "Whether to use code review", value = False)
-            staffs = gr.CheckboxGroup(["ProjectManager", "ProductManager", "Architect", "Engineer", "QaEngineer"], label="Choose the staff you would like to hire", value = ["ProjectManager", "ProductManager", "Architect", "Engineer"])
+            staffs = gr.CheckboxGroup(["ProjectManager", "ProductManager", "Architect", "Engineer", "QaEngineer"],\
+                                        label="Choose the staff you would like to hire",\
+                                        value = ["ProjectManager", "ProductManager", "Architect", "Engineer"])
             idea = gr.Textbox(label="Your innovative idea, such as 'Creating a snake game.'", value = "Creating a snake game.")
             with gr.Row():
                 Start_MetaGPT = gr.Button(label="Start / ReStart", value = "Start / ReStart")
@@ -114,16 +128,24 @@ with app:
                 output_sent_from_metagpt = gr.Markdown(label="The sent_from of the output of MetaGPT")
                 output_send_to_metagpt = gr.Markdown(label="The send_to of the output of MetaGPT")
             with gr.Row():
-                output_content_metagpt = gr.Textbox(label="The phased output of MetaGPT, modify it as your will",max_lines=999,show_copy_button = True)
+                output_content_metagpt = gr.Textbox(label="The phased output of MetaGPT, modify it as your will",\
+                                                    max_lines=999,show_copy_button = True)
                 output_content_markdown_metagpt = gr.Markdown(label="The markdown output of MetaGPT", visible = False)
             
             
-        Start_MetaGPT.click(startup, [company_choise, idea, investment, n_round, code_review, staffs], [output_role_metagpt,output_cause_metagpt,output_sent_from_metagpt,output_send_to_metagpt,output_content_metagpt,output_content_markdown_metagpt])
+        Start_MetaGPT.click(startup, [company_choise, idea, investment, n_round, code_review, staffs],\
+            [output_role_metagpt,output_cause_metagpt,output_sent_from_metagpt,output_send_to_metagpt,\
+            output_content_metagpt,output_content_markdown_metagpt])
         # clear_log.click(clear_logs, [],[])
-        continue_run.click(__continue, [output_content_metagpt], [output_role_metagpt,output_cause_metagpt,output_sent_from_metagpt,output_send_to_metagpt,output_content_metagpt,output_content_markdown_metagpt])
-        company_choise.change(lambda company_choise : gr.update(visible = True if company_choise == "SoftwareCompany_With_Human" else False), [company_choise], [continue_run])
-        company_choise.change(lambda company_choise : gr.update(visible = False if company_choise == "SoftwareCompany_With_Human" else True), [company_choise], [n_round])
-        show_markdown.change(lambda x: gr.update(visible = True if x == True else False), [show_markdown], [output_content_markdown_metagpt])
+        continue_run.click(__continue, [output_content_metagpt],\
+            [output_role_metagpt,output_cause_metagpt,output_sent_from_metagpt,output_send_to_metagpt,\
+            output_content_metagpt,output_content_markdown_metagpt])
+        company_choise.change(lambda company_choise : gr.update(visible = True \
+            if company_choise == "SoftwareCompany_With_Human" else False), [company_choise], [continue_run])
+        company_choise.change(lambda company_choise : gr.update(visible = False \
+            if company_choise == "SoftwareCompany_With_Human" else True), [company_choise], [n_round])
+        show_markdown.change(lambda x: gr.update(visible = True if x == True else False),\
+            [show_markdown], [output_content_markdown_metagpt])
         with gr.TabItem("Research") as research_tab:
             language = gr.Dropdown(label = "Choose the language", choices = ["en-us","zh-ch"], value = "en-us")
             topic = gr.Textbox(label="Your research topic, such as 'dataiku vs. datarobot'", value = "dataiku vs. datarobot")
