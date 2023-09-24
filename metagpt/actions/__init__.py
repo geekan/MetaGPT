3f892482a2bc5fd4cf357962ec7f985549cfadfa
stellahsr <stellahsr@126.com>
2023-09-24 11:20:20 +0800

    在当前branch 清除其他无关的actions

--- metagpt/actions/__init__.py
+++ metagpt/actions/__init__.py
@@ -12,16 +12,12 @@ from metagpt.actions.action_output import ActionOutput
 from metagpt.actions.add_requirement import BossRequirement
 from metagpt.actions.debug_error import DebugError
 from metagpt.actions.design_api import WriteDesign
-from metagpt.actions.design_api_review import DesignReview
-from metagpt.actions.design_filenames import DesignFilenames
 from metagpt.actions.project_management import AssignTasks, WriteTasks
-from metagpt.actions.research import CollectLinks, WebBrowseAndSummarize, ConductResearch
 from metagpt.actions.run_code import RunCode
 from metagpt.actions.search_and_summarize import SearchAndSummarize
 from metagpt.actions.write_code import WriteCode
 from metagpt.actions.write_code_review import WriteCodeReview
 from metagpt.actions.write_prd import WritePRD
-from metagpt.actions.write_prd_review import WritePRDReview
 from metagpt.actions.write_test import WriteTest
 
 
@@ -30,10 +26,7 @@ class ActionType(Enum):
 
     ADD_REQUIREMENT = BossRequirement
     WRITE_PRD = WritePRD
-    WRITE_PRD_REVIEW = WritePRDReview
     WRITE_DESIGN = WriteDesign
-    DESIGN_REVIEW = DesignReview
-    DESIGN_FILENAMES = DesignFilenames
     WRTIE_CODE = WriteCode
     WRITE_CODE_REVIEW = WriteCodeReview
     WRITE_TEST = WriteTest
@@ -42,9 +35,6 @@ class ActionType(Enum):
     WRITE_TASKS = WriteTasks
     ASSIGN_TASKS = AssignTasks
     SEARCH_AND_SUMMARIZE = SearchAndSummarize
-    COLLECT_LINKS = CollectLinks
-    WEB_BROWSE_AND_SUMMARIZE = WebBrowseAndSummarize
-    CONDUCT_RESEARCH = ConductResearch
 
 
 __all__ = [
