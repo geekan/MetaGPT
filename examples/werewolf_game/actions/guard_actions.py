c9dca2a04ba5cb595c35fd9f25ef5fe3780c241e
kevin-meng <meng_fr595@163.com>
2023-09-25 23:27:12 +0800

    modify pronunciation

--- examples/werewolf_game/actions/guard_actions.py
+++ examples/werewolf_game/actions/guard_actions.py
@@ -9,10 +9,10 @@ class Protect(Action):
     this is game history:
     {context}.
     Attention: you can not protect the same player two nights in a row.  
-    Now, choose one to portect, you will:
+    Now, choose one to protect, you will:
     """
 
-    def __init__(self, name="Speak", context=None, llm=None):
+    def __init__(self, name="Protect", context=None, llm=None):
         super().__init__(name, context, llm)
 
     async def run(self, context: str):
