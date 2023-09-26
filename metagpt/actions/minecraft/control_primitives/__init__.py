b090a628a0741562f7546b53a1f82276c92cb197
向劲宇 <137690584+isaacJinyu@users.noreply.github.com>
2023-09-27 02:13:36 +0800

    Update __init__.py

--- metagpt/actions/minecraft/control_primitives/__init__.py
+++ metagpt/actions/minecraft/control_primitives/__init__.py
@@ -4,15 +4,15 @@ import voyager.utils as U
 
 
 def load_control_primitives(primitive_names=None):
-    package_path = pkg_resources.resource_filename("voyager", "")
+    package_path = pkg_resources.resource_filename("metagpt", "")
     if primitive_names is None:
         primitive_names = [
             primitives[:-3]
-            for primitives in os.listdir(f"{package_path}/control_primitives")
+            for primitives in os.listdir(f"{package_path}/actions/minecraft/control_primitives")
             if primitives.endswith(".js")
         ]
     primitives = [
-        U.load_text(f"{package_path}/control_primitives/{primitive_name}.js")
+        U.load_text(f"{package_path}/actions/minecraft/control_primitives/{primitive_name}.js")
         for primitive_name in primitive_names
     ]
     return primitives
