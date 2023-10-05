1ec2b922c13c87cc9d48c08a1f3246f43c2386e2
didi <2020201387@ruc.edu.cn>
2023-10-05 19:45:37 +0800

    更新需要测试Agent类

--- examples/st_game/tests/test_reflect.py
+++ examples/st_game/tests/test_reflect.py
@@ -1,7 +1,7 @@
 
 import pytest
 from examples.st_game.roles.st_role import STRole
-from examples.st_game.actions.run_reflect_action import AgentFocusPt
+from examples.st_game.actions.run_reflect_action import AgentFocusPt, AgentInsightAndGuidance, AgentEventTriple, AgentEventPoignancy, AgentChatPoignancy, AgentPlanThoughtOnConvo, AgentMemoryOnConvo
 from metagpt.logs import logger
 
 
@@ -14,13 +14,35 @@ class TestReflectFunction:
         logger.info(f"记忆长度为{len(role.memory.storage)}")
         return role
 
-    def test_fuction_point_action(self,init_agent):
+    def test_function_focus_and_insight_action(self,init_agent):
         """
-
+        单个Action测试样例
         """
         run_focus = AgentFocusPt()
         statements = "" # 这个statements 与 n 设置是遵循reflect里面实际设置# 来的，你写的时候可以对应代码看一下
         run_focus.run(init_agent, statements, n=3)
+
+        run_insight = AgentInsightAndGuidance()
+        # 这里主要需要查看，Looger.info(filling)
+        # 完善代码
+
+    def test_event_triple_action(self,init_agent):
+        """
+        测试tripleAgent Action
+        """
+        pass
+
+    def test_poignancy_action(self,init_agent):
+        """
+        测试两个关于poignancy的Action
+        """
+        pass
+
+    def test_convo_action(self,init_agent):
+        """
+        测试两个convo相关的类
+        """
+        pass
 
     # 测试全部Reflection功能
     def test_reflect_function(self, init_agent):
