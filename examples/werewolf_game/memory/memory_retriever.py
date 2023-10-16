1d8177914d957ff0c8537855b6afe585b4a744fe
mannaandpoem <1580466765@qq.com>
2023-10-16 18:40:04 +0800

    update MemoryRetriever

--- examples/werewolf_game/memory/memory_retriever.py
+++ examples/werewolf_game/memory/memory_retriever.py
@@ -1,5 +1,7 @@
 import re
 
+from metagpt.const import WORKSPACE_ROOT
+
 
 class MemoryRetriever:
     def __init__(self, mode="full"):
@@ -17,17 +19,21 @@ class MemoryRetriever:
 
         recent_m_memories = memory[-m:]  # 取最近m条记忆
 
-        # 将所有记忆按照重要性打分
-        scored_memories = [(message, self.score_message(message.content, profile, name)) for message in memory]
-
-        # 提取分数最高的n条记忆，如果分数相同，则较新轮次的记忆排在前面
-        # sorted_memories = sorted(scored_memories, key=lambda x: x[1], reverse=True)
-        sorted_memories = sorted(scored_memories, key=lambda x: (x[1], scored_memories.index(x)),
-                                 reverse=True)
+        before_k_memories = memory[:-m]  # 取最近m条之前的记忆
+        # 对最近k条之前的记忆按照重要性打分
+        scored_memories = [(message, self.score_message(message.content, profile, name)) for message in
+                           before_k_memories]
+        scored_memories = [(message, self.score_message(message.content, profile, name)) for message in
+                           recent_m_memories]
+        # 提取分数最高的n条记忆
+        sorted_memories = sorted(scored_memories, key=lambda x: x[1], reverse=True)
+        # sorted_memories = sorted(scored_memories, key=lambda x: (x[1], scored_memories.index(x)), reverse=True)
 
         # 如果informative记忆与recent记忆不重合，则保留。如果重合，若该消息优先级大于3，则保留，否则不保留
-        top_n_informative_memories = [memory[0] for memory in sorted_memories[:n] if
-                                      memory[0] not in recent_m_memories or memory[1] > 3]
+        # top_n_informative_memories = [memory[0] for memory in sorted_memories[:n] if
+        #                               memory[0] not in recent_m_memories or memory[1] > 3]
+
+        top_n_informative_memories = [memory[0] for memory in sorted_memories[:n]]
 
         time_stamp_pattern = r'[0-9]+ \| '
 
@@ -36,10 +42,16 @@ class MemoryRetriever:
         informative_memories = [f"{m.sent_from}: {re.sub(time_stamp_pattern, '', m.content)}" for m in
                                 top_n_informative_memories]
 
-        memories = ("Two kinds of messages: Recent messages(The priority is from low to high) and "
-                    "Informative messages(The priority is from high to low)\n"
+        memories = ("Two kinds of messages: Recent messages(last k memories) and "
+                    "Informative messages(before the last k memories)\n"
                     "Recent Messages:\n" + "\n".join(recent_memories) +
                     "\n\nInformative Messages:\n" + "\n".join(informative_memories))
+
+        # 记录memories
+        if len(sorted_memories) % 10 == 0:
+            filename = f"{profile}_memories.txt"
+            with open(WORKSPACE_ROOT / filename, "a") as f:
+                f.write(memories + "\n\n得分情况：\n" + str(sorted_memories))
 
         return memories
 
