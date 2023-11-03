bdf59b67bd4bfe5b381b9c61cf59086af01127c5
莘权 马 <mashenquan@fuzhi.ai>
2023-11-03 12:02:52 +0800

    refactor: update notations

--- tests/metagpt/actions/test_write_prd.py
+++ tests/metagpt/actions/test_write_prd.py
@@ -4,7 +4,8 @@
 @Time    : 2023/5/11 17:45
 @Author  : alexanderwu
 @File    : test_write_prd.py
-@Modified By: mashenquan, 2023-11-1. Standardize the usage of message filtering-related features.
+@Modified By: mashenquan, 2023-11-1. According to Chapter 2.2.1 and 2.2.2 of RFC 116, change the data type of
+        the `cause_by` value in the `Message` to a string to support the new message distribution feature.
 """
 import pytest
 
@@ -18,7 +19,7 @@ from metagpt.schema import Message
 async def test_write_prd():
     product_manager = ProductManager()
     requirements = "开发一个基于大语言模型与私有知识库的搜索引擎，希望可以基于大语言模型进行搜索总结"
-    prd = await product_manager.handle(Message(content=requirements, cause_by=BossRequirement.get_class_name()))
+    prd = await product_manager.run(Message(content=requirements, cause_by=BossRequirement.get_class_name()))
     logger.info(requirements)
     logger.info(prd)
 
