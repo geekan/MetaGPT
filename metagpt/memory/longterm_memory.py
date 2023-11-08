894a2fd593734c7e8611b0033304884dac6d9397
莘权 马 <mashenquan@fuzhi.ai>
2023-11-08 23:36:56 +0800

    refactor: Update according to Chapter 2.1.3.2 of RFC 116

--- metagpt/memory/longterm_memory.py
+++ metagpt/memory/longterm_memory.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 @Desc   : the implement of Long-term memory
-@Modified By: mashenquan, 2023-11-1. According to Chapter 2.2.1 and 2.2.2 of RFC 116:
-    1. Replace code related to message filtering with the `Message.contain_any` function.
 """
 
 from metagpt.logs import logger
