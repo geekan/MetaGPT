d9a2626fde3c7e43e7e118e3ee740a0ad4b9fcf9
莘权 马 <mashenquan@fuzhi.ai>
2023-11-21 13:39:10 +0800

    feat: +PrepareDocuments

---
+++ metagpt/actions/prepare_documents.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2023/11/20
+@Author  : mashenquan
+@File    : git_repository.py
+@Desc: PrepareDocuments Action: initialize project folder and add new requirements to docs/requirements.txt.
+        RFC 135 2.2.3.5.1.
+"""
+from metagpt.actions import Action
+
+
+class PrepareDocuments(Action):
+    def __init__(self, name="", context=None, llm=None):
+        pass
