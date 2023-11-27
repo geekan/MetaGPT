1496af273a45db1d47b3e75d5a1e4497380f113d
stellahsr <stellahsr@126.com>
2023-11-27 11:32:23 +0800

    update serialize and deserialize

---
+++ tests/metagpt/provider/test_spark_api.py
@@ -0,0 +1,9 @@
+from metagpt.logs import logger
+from metagpt.provider.spark_api import SparkAPI
+
+
+def test_message():
+    llm = SparkAPI()
+
+    logger.info(llm.ask('只回答"收到了"这三个字。'))
+    logger.info(llm.ask('写一篇五百字的日记'))
