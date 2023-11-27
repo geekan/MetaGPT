16226a2e11621b91d05feb4a74e97259f95d66b1
莘权 马 <mashenquan@fuzhi.ai>
2023-11-27 15:35:22 +0800

    feat: +annotation

--- metagpt/actions/write_prd.py
+++ metagpt/actions/write_prd.py
@@ -4,6 +4,9 @@
 @Time    : 2023/5/11 17:45
 @Author  : alexanderwu
 @File    : write_prd.py
+@Modified By: mashenquan, 2023/11/27.
+            1. According to Section 2.2.3.1 of RFC 135, replace file data in the message with the file name.
+            2. According to the design in Section 2.2.3.5.2 of RFC 135, add incremental iteration functionality.
 """
 from __future__ import annotations
 
