3d0911070c9780091026817ca99db29b6c917c68
YangQianli92 <108046369+YangQianli92@users.noreply.github.com>
2023-11-28 10:17:54 +0800

    Add Multi-LLM Configuration

--- metagpt/config.py
+++ metagpt/config.py
@@ -43,13 +43,13 @@ class Config(metaclass=Singleton):
         self._init_with_config_files_and_env(self._configs, yaml_file)
         logger.info("Config loading done.")
         self.global_proxy = self._get("GLOBAL_PROXY")
-        self.openai_api_key = self._get("OPENAI_API_KEY"," ")
+        self.openai_api_key = self._get("OPENAI_API_KEY")
         self.anthropic_api_key = self._get("Anthropic_API_KEY")
         self.zhipuai_api_key = self._get("ZHIPUAI_API_KEY")
-        # if (not self.openai_api_key or "YOUR_API_KEY" == self.openai_api_key) and \
-        #         (not self.anthropic_api_key or "YOUR_API_KEY" == self.anthropic_api_key) and \
-        #         (not self.zhipuai_api_key or "YOUR_API_KEY" == self.zhipuai_api_key):
-        #     raise NotConfiguredException("Set OPENAI_API_KEY or Anthropic_API_KEY or ZHIPUAI_API_KEY first")
+        if (not self.openai_api_key or "YOUR_API_KEY" == self.openai_api_key) and \
+                (not self.anthropic_api_key or "YOUR_API_KEY" == self.anthropic_api_key) and \
+                (not self.zhipuai_api_key or "YOUR_API_KEY" == self.zhipuai_api_key):
+            raise NotConfiguredException("Set OPENAI_API_KEY or Anthropic_API_KEY or ZHIPUAI_API_KEY first")
         self.openai_api_base = self._get("OPENAI_API_BASE")
         openai_proxy = self._get("OPENAI_PROXY") or self.global_proxy
         if openai_proxy:
