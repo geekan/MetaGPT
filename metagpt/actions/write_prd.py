e34b8bbf0bbb9d46757da1eb33883fd8b554df56
莘权 马 <mashenquan@fuzhi.ai>
2023-11-29 22:13:10 +0800

    feat: merge geekan:cli-etc

--- metagpt/actions/write_prd.py
+++ metagpt/actions/write_prd.py
@@ -261,24 +261,41 @@ MERGE_PROMPT = """
 {old_prd}
 -----
 Role: You are a professional product manager; The goal is to merge the newly added requirements into the existing PRD in order to design a concise, usable, and efficient product.
+Language: Please use the same language as the user requirement, but the title and code should be still in English. For example, if the user speaks Chinese, the specific text of your answer should also be in Chinese.
 Requirements: According to the context, fill in the following missing information, each section name is a key in json ,If the requirements are unclear, ensure minimum viability and avoid excessive design
+ATTENTION: Output carefully referenced "Old PRD" in format.
 
-## Original Requirements: Provide as Plain text, place the polished complete original requirements here
+## YOU NEED TO FULFILL THE BELOW JSON DOC
 
-## Product Goals: Provided as Python list[str], up to 3 clear, orthogonal product goals. If the requirement itself is simple, the goal should also be simple
-
-## User Stories: Provided as Python list[str], up to 5 scenario-based user stories, If the requirement itself is simple, the user stories should also be less
-
-## Competitive Analysis: Provided as Python list[str], up to 7 competitive product analyses, consider as similar competitors as possible
-
-## Competitive Quadrant Chart: Use mermaid quadrantChart code syntax. up to 14 competitive products. Translation: Distribute these competitor scores evenly between 0 and 1, trying to conform to a normal distribution centered around 0.5 as much as possible.
-
-## Requirement Analysis: Provide as Plain text. Be simple. LESS IS MORE. Make your requirements less dumb. Delete the parts unnessasery.
-
-## Requirement Pool: Provided as Python list[list[str], the parameters are requirement description, priority(P0/P1/P2), respectively, comply with PEP standards; no more than 5 requirements and consider to make its difficulty lower
-
-## UI Design draft: Provide as Plain text. Be simple. Describe the elements and functions, also provide a simple style description and layout description.
-## Anything UNCLEAR: Provide as Plain text. Make clear here.
+{{
+    "Language": "", # str, use the same language as the user requirement. en_us / zh_cn etc.
+    "Original Requirements": "", # str, place the polished complete original requirements here
+    "project_name": "", # str, name it like game_2048 / web_2048 / simple_crm etc.
+    "Search Information": "",
+    "Requirements": "", 
+    "Product Goals": [], # Provided as Python list[str], up to 3 clear, orthogonal product goals.
+    "User Stories": [], # Provided as Python list[str], up to 5 scenario-based user stories
+    "Competitive Analysis": [], # Provided as Python list[str], up to 8 competitive product analyses
+    # Use mermaid quadrantChart code syntax. up to 14 competitive products. Translation: Distribute these competitor scores evenly between 0 and 1, trying to conform to a normal distribution centered around 0.5 as much as possible.
+    "Competitive Quadrant Chart": "quadrantChart
+                title Reach and engagement of campaigns
+                x-axis Low Reach --> High Reach
+                y-axis Low Engagement --> High Engagement
+                quadrant-1 We should expand
+                quadrant-2 Need to promote
+                quadrant-3 Re-evaluate
+                quadrant-4 May be improved
+                Campaign A: [0.3, 0.6]
+                Campaign B: [0.45, 0.23]
+                Campaign C: [0.57, 0.69]
+                Campaign D: [0.78, 0.34]
+                Campaign E: [0.40, 0.34]
+                Campaign F: [0.35, 0.78]",
+    "Requirement Analysis": "", # Provide as Plain text.
+    "Requirement Pool": [["P0","P0 requirement"],["P1","P1 requirement"]], # Provided as Python list[list[str], the parameters are requirement description, priority(P0/P1/P2), respectively, comply with PEP standards
+    "UI Design draft": "", # Provide as Plain text. Be simple. Describe the elements and functions, also provide a simple style description and layout description.
+    "Anything UNCLEAR": "", # Provide as Plain text. Try to clarify it.
+}}
 
 output a properly formatted JSON, wrapped inside [CONTENT][/CONTENT] like "Old PRD" format,
 and only output the json inside this tag, nothing else
