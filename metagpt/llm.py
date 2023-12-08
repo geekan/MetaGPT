3b8363cffb55917237ea779f7ceb8b315224056f
paulaan <me@paulaan.me>
2023-12-08 13:01:03 +0700

    fix timeout

--- metagpt/llm.py
+++ metagpt/llm.py
@@ -10,7 +10,6 @@ from metagpt.logs import logger
 from metagpt.config import CONFIG
 from metagpt.provider.anthropic_api import Claude2 as Claude
 from metagpt.provider.openai_api import OpenAIGPTAPI
-from metagpt.provider.zhipuai_api import ZhiPuAIGPTAPI
 from metagpt.provider.spark_api import SparkAPI
 from metagpt.provider.human_provider import HumanProvider
 
@@ -24,8 +23,6 @@ def LLM() -> "BaseGPTAPI":
         llm = Claude()
     elif CONFIG.spark_api_key:
         llm = SparkAPI()
-    elif CONFIG.zhipuai_api_key:
-        llm = ZhiPuAIGPTAPI()
     else:
         raise RuntimeError("You should config a LLM configuration first")
 
