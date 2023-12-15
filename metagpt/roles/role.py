862707d4b7bd319873e550010253a8df0844f6b8
geekan <geekan@foxmail.com>
2023-12-15 10:56:08 +0800

    use react instead of _react

--- metagpt/roles/role.py
+++ metagpt/roles/role.py
@@ -412,7 +412,7 @@ class Role:
             logger.debug(f"{self._setting}: no news. waiting.")
             return
 
-        rsp = await self._react()
+        rsp = await self.react()
 
         # Reset the next action to be taken.
         self._rc.todo = None
