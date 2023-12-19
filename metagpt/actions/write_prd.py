09e2f05a6a553c32cfdcdb53ec680d73acda1af2
geekan <geekan@foxmail.com>
2023-12-19 21:24:08 +0800

    refactor action_output and action_node

--- metagpt/actions/write_prd.py
+++ metagpt/actions/write_prd.py
@@ -67,7 +67,7 @@ class WritePRD(Action):
     def __init__(self, name="", context=None, llm=None):
         super().__init__(name, context, llm)
 
-    async def run(self, with_messages, format=CONFIG.prompt_format, *args, **kwargs) -> ActionOutput | Message:
+    async def run(self, with_messages, schema=CONFIG.prompt_schema, *args, **kwargs) -> ActionOutput | Message:
         # Determine which requirement documents need to be rewritten: Use LLM to assess whether new requirements are
         # related to the PRD. If they are related, rewrite the PRD.
         docs_file_repo = CONFIG.git_repo.new_file_repository(relative_path=DOCS_FILE_REPO)
