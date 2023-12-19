fc829edc45571f9ca3b5d3212a4f49e46d77a4eb
geekan <geekan@foxmail.com>
2023-12-19 18:04:12 +0800

    make registry work

--- metagpt/llm.py
+++ metagpt/llm.py
@@ -6,7 +6,7 @@
 @File    : llm.py
 """
 
-from metagpt.config import CONFIG
+from metagpt.config import CONFIG, LLMProviderEnum
 from metagpt.provider.base_gpt_api import BaseGPTAPI
 from metagpt.provider.human_provider import HumanProvider
 from metagpt.provider.llm_provider_registry import LLM_REGISTRY
@@ -14,7 +14,6 @@ from metagpt.provider.llm_provider_registry import LLM_REGISTRY
 _ = HumanProvider()  # Avoid pre-commit error
 
 
-def LLM() -> BaseGPTAPI:
-    """initialize different LLM instance according to the key field existence"""
-    # TODO a little trick, can use registry to initialize LLM instance further
-    return LLM_REGISTRY.get_provider(CONFIG.get_default_llm_provider_enum())
+def LLM(provider: LLMProviderEnum = CONFIG.get_default_llm_provider_enum()) -> BaseGPTAPI:
+    """get the default llm provider"""
+    return LLM_REGISTRY.get_provider(provider)
