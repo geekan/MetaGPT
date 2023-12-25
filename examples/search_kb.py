bd1014e19ab1c2fcad316690a8f6bacbde47d1cb
geekan <geekan@foxmail.com>
2023-12-25 22:13:36 +0800

    add sales test

--- examples/search_kb.py
+++ examples/search_kb.py
@@ -6,28 +6,18 @@
 """
 import asyncio
 
-from langchain.embeddings import OpenAIEmbeddings
-
-from metagpt.config import CONFIG
 from metagpt.const import EXAMPLE_PATH
 from metagpt.document_store import FaissStore
 from metagpt.logs import logger
 from metagpt.roles import Sales
 
 
-def get_store():
-    embedding = OpenAIEmbeddings(openai_api_key=CONFIG.openai_api_key, openai_api_base=CONFIG.openai_base_url)
-    return FaissStore(EXAMPLE_PATH / "example.json", embedding=embedding)
-
-
 async def search():
-    role = Sales(profile="Sales", store=get_store())
-    queries = ["Which facial cleanser is good for oily skin?", "Is L'Oreal good to use?"]
-
-    for query in queries:
-        logger.info(f"User: {query}")
-        result = await role.run(query)
-        logger.info(result)
+    store = FaissStore(EXAMPLE_PATH / "example.json")
+    role = Sales(profile="Sales", store=store)
+    query = "Which facial cleanser is good for oily skin?"
+    result = await role.run(query)
+    logger.info(result)
 
 
 if __name__ == "__main__":
