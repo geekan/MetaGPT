89b6f2935a071defc2566fe460f39bb91ad0ed93
zhongyang <zhongyang@fuzhi.ai>
2024-01-04 16:32:14 +0800

    update aget

--- metagpt/utils/ahttp_client.py
+++ metagpt/utils/ahttp_client.py
@@ -28,6 +28,24 @@ async def apost(
     return data
 
 
+async def aget(
+    url: str,
+    params: Optional[Mapping[str, str]] = None,
+    headers: Optional[dict] = None,
+    as_json: bool = False,
+    encoding: str = "utf-8",
+    timeout: int = DEFAULT_TIMEOUT.total,
+) -> Union[str, dict]:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=url, params=params, headers=headers, timeout=timeout) as resp:
+            if as_json:
+                data = await resp.json()
+            else:
+                data = await resp.read()
+                data = data.decode(encoding)
+    return data
+
+
 async def apost_stream(
     url: str,
     params: Optional[Mapping[str, str]] = None,
