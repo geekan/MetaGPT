2289763dfe5dee6393a00683070b8ca4de8d74aa
better629 <webdesign_xmu2012@163.com>
2024-01-23 16:35:51 +0800

    update stanford_town_ext_env

--- metagpt/environment/stanford_town_env/stanford_town_env.py
+++ metagpt/environment/stanford_town_env/stanford_town_env.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 # @Desc   : MG StanfordTown Env
 
-from metagpt.env.stanford_town_env.stanford_town_ext_env import StanfordTownExtEnv
+from metagpt.environment.stanford_town_env.stanford_town_ext_env import (
+    StanfordTownExtEnv,
+)
 
 
 class StanfordTownEnv(StanfordTownExtEnv):
