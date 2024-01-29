194744348acfd7727c54689a6d2e0813b32be4df
didi <2020201387@ruc.edu.cn>
2024-01-29 23:47:06 +0800

    Create test.py

---
+++ examples/andriod_assistant/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Desc   : test case (imgs from appagent's)
+
+from metagpt.actions.action import Action
+from examples.andriod_assistant.utils.schema import AndroidActionOutput
+
+# TODO test for manual record
+
+# TODO test for parse record
+
+# TODO test for screenshot_parse
+
+# TODO add AndroidActionOutput to manual record & parse record
+
