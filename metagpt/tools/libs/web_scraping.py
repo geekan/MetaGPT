9b72370cbebda21d0ad120ef0f42bc1199cb7922
yzlin <yzlin@fuzhi.ai>
2024-02-05 22:15:47 +0800

    update webscraping tool

--- metagpt/tools/libs/web_scraping.py
+++ metagpt/tools/libs/web_scraping.py
@@ -4,19 +4,18 @@ from metagpt.tools.web_browser_engine_playwright import PlaywrightWrapper
 
 
 @register_tool(tool_type=ToolType.WEBSCRAPING.type_name)
-async def scrape_web_playwright(url, *urls):
+async def scrape_web_playwright(url):
     """
-    Scrape and save the HTML structure and inner text content of a web page using Playwright.
+    Asynchronously Scrape and save the HTML structure and inner text content of a web page using Playwright.
 
     Args:
         url (str): The main URL to fetch inner text from.
-        *urls (str): Additional URLs to fetch inner text from.
 
     Returns:
-        (dict): The inner text content and html structure of the web page, key are : 'inner_text', 'html'.
+        dict: The inner text content and html structure of the web page, keys are 'inner_text', 'html'.
     """
     # Create a PlaywrightWrapper instance for the Chromium browser
-    web = await PlaywrightWrapper().run(url, *urls)
+    web = await PlaywrightWrapper().run(url)
 
     # Return the inner text content of the web page
     return {"inner_text": web.inner_text.strip(), "html": web.html.strip()}
