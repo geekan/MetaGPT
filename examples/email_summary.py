4a991b4cd4eb0bdfb415b8ffb8bb02c3d33f10e2
Zhou Tuo <45249333+FromCSUZhou@users.noreply.github.com>
2024-02-07 07:59:54 +0000

    add email_login unit test

--- examples/email_summary.py
+++ examples/email_summary.py
@@ -9,14 +9,15 @@ from metagpt.roles.ci.code_interpreter import CodeInterpreter
 
 
 async def main():
-    # prompt_response = """I will give you your Outlook email account(englishgpt@outlook.com) and password(the outlook_email_password item in the environment variable). You need to find the latest email in my inbox with the sender's suffix @qq.com and reply to him "Thank you! I have received your email~"""""
-    prompt_summary = """I will give you your Outlook email account(englishgpt@outlook.com) and password(outlook_email_password item in the environment variable).
+    # For email response prompt
+    # prompt = """I will give you your Outlook email account(englishgpt@outlook.com) and password(the outlook_email_password item in the environment variable). You need to find the latest email in my inbox with the sender's suffix @qq.com and reply to him "Thank you! I have received your email~"""""
+    prompt = """I will give you your Outlook email account(englishgpt@outlook.com) and password(outlook_email_password item in the environment variable).
             Firstly, Please help me present the latest 5 senders and full letter contents.
             Then, summarize each of the 5 emails into one sentence with Chinese(you can do this by yourself, don't need import other models to do this) and output them in a markdown format."""
-    # ci_response = CodeInterpreter(goal=prompt_response, use_tools=True)
-    ci_summary = CodeInterpreter(goal=prompt_summary, use_tools=True)
 
-    await ci_summary.run(prompt_summary)
+    ci = CodeInterpreter(use_tools=True)
+
+    await ci.run(prompt)
 
 
 if __name__ == "__main__":
--- metagpt/tools/libs/email_login.py
+++ metagpt/tools/libs/email_login.py
@@ -1,7 +1,28 @@
 from imap_tools import MailBox
 
+from metagpt.logs import logger
 from metagpt.tools.tool_registry import register_tool
 from metagpt.tools.tool_type import ToolType
+
+# Define a dictionary mapping email domains to their IMAP server addresses
+IMAP_SERVERS = {
+    "outlook.com": "imap-mail.outlook.com",  # Outlook
+    "163.com": "imap.163.com",  # 163 Mail
+    "qq.com": "imap.qq.com",  # QQ Mail
+    "gmail.com": "imap.gmail.com",  # Gmail
+    "yahoo.com": "imap.mail.yahoo.com",  # Yahoo Mail
+    "icloud.com": "imap.mail.me.com",  # iCloud Mail
+    "hotmail.com": "imap-mail.outlook.com",  # Hotmail (同 Outlook)
+    "live.com": "imap-mail.outlook.com",  # Live (同 Outlook)
+    "sina.com": "imap.sina.com",  # Sina Mail
+    "sohu.com": "imap.sohu.com",  # Sohu Mail
+    "yahoo.co.jp": "imap.mail.yahoo.co.jp",  # Yahoo Mail Japan
+    "yandex.com": "imap.yandex.com",  # Yandex Mail
+    "mail.ru": "imap.mail.ru",  # Mail.ru
+    "aol.com": "imap.aol.com",  # AOL Mail
+    "gmx.com": "imap.gmx.com",  # GMX Mail
+    "zoho.com": "imap.zoho.com",  # Zoho Mail
+}
 
 
 @register_tool(tool_type=ToolType.EMAIL_LOGIN.type_name)
@@ -17,34 +38,14 @@ def email_login_imap(email_address, email_password):
         object: The imap_tools's MailBox object returned after successfully connecting to the mailbox through imap_tools, including various information about this account (email, etc.), or None if login fails.
     """
 
-    # Define a dictionary mapping email domains to their IMAP server addresses
-    imap_servers = {
-        "outlook.com": "imap-mail.outlook.com",  # Outlook
-        "163.com": "imap.163.com",  # 163 Mail
-        "qq.com": "imap.qq.com",  # QQ Mail
-        "gmail.com": "imap.gmail.com",  # Gmail
-        "yahoo.com": "imap.mail.yahoo.com",  # Yahoo Mail
-        "icloud.com": "imap.mail.me.com",  # iCloud Mail
-        "hotmail.com": "imap-mail.outlook.com",  # Hotmail (同 Outlook)
-        "live.com": "imap-mail.outlook.com",  # Live (同 Outlook)
-        "sina.com": "imap.sina.com",  # Sina Mail
-        "sohu.com": "imap.sohu.com",  # Sohu Mail
-        "yahoo.co.jp": "imap.mail.yahoo.co.jp",  # Yahoo Mail Japan
-        "yandex.com": "imap.yandex.com",  # Yandex Mail
-        "mail.ru": "imap.mail.ru",  # Mail.ru
-        "aol.com": "imap.aol.com",  # AOL Mail
-        "gmx.com": "imap.gmx.com",  # GMX Mail
-        "zoho.com": "imap.zoho.com",  # Zoho Mail
-    }
-
     # Extract the domain from the email address
     domain = email_address.split("@")[-1]
 
     # Determine the correct IMAP server
-    imap_server = imap_servers.get(domain)
+    imap_server = IMAP_SERVERS.get(domain)
 
     if not imap_server:
-        print(f"IMAP server for {domain} not found.")
+        logger.error(f"IMAP server for {domain} not found.")
         return None
 
     # Attempt to log in to the email account
