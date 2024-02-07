d29ab799bf7de735a76754e85d662cbbd8e5525e
yzlin <yzlin@fuzhi.ai>
2024-02-07 15:55:20 +0800

    change math problems

--- examples/ci/solve_math_problems.py
+++ examples/ci/solve_math_problems.py
@@ -4,14 +4,10 @@ from metagpt.roles.ci.code_interpreter import CodeInterpreter
 
 
 async def main(requirement: str = ""):
-    code_interpreter = CodeInterpreter(use_tools=False, goal=requirement)
+    code_interpreter = CodeInterpreter(use_tools=False)
     await code_interpreter.run(requirement)
 
 
 if __name__ == "__main__":
-    problem = "At a school, all 60 students play on at least one of three teams: Basketball, Soccer, and Mathletics. 8 students play all three sports, half the students play basketball, and the ratio of the size of the math team to the size of the basketball team to the size of the soccer team is $4:3:2$. How many students at the school play on exactly two teams?"
-    requirement = (
-        f"This is a math problem:{problem}. You can analyze and solve it step by step or use Python code to solve it."
-    )
-
+    requirement = "Solve this math problem: The greatest common divisor of positive integers m and n is 6. The least common multiple of m and n is 126. What is the least possible value of m + n?"
     asyncio.run(main(requirement))
