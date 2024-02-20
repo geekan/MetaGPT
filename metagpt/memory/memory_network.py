fae24fd38144cc6b2c709dedd703ca11921092aa
seehi <6580@pm.me>
2024-02-20 17:08:50 +0800

    fix by pre-commit hooks

--- metagpt/memory/memory_network.py
+++ metagpt/memory/memory_network.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 # @Desc   : the memory network to store memory segment
 
-from pydantic import Field, BaseModel
+from pydantic import BaseModel, Field
 
-from metagpt.memory.schema import MemorySegment, MemoryNode
+from metagpt.memory.schema import MemoryNode, MemorySegment
 
 
 class MemoryNetwork(BaseModel):
-    mem_seg: MemorySegment = Field(default_factory=MemorySegment, description="the memory segment to store memory nodes")
+    mem_seg: MemorySegment = Field(
+        default_factory=MemorySegment, description="the memory segment to store memory nodes"
+    )
 
     def add_mem(self, mem_node: MemoryNode):
         self.mem_seg.add_mem_node(mem_node)
