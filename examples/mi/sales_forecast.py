41ac613a8c9558029790333ed0df28879579a897
刘棒棒 <liubangbang@fuzhi.ai>
2024-02-20 20:01:30 +0800

    add new example, sales_forecast.

---
+++ examples/mi/sales_forecast.py
@@ -0,0 +1,36 @@
+from metagpt.roles.mi.interpreter import Interpreter
+
+
+async def main():
+    # data from https://www.kaggle.com/datasets/aslanahmedov/walmart-sales-forecast/data
+    train_data = "examples/mi/data/WalmartSalesForecast/new_train.csv"
+    test_data = "examples/mi/data/WalmartSalesForecast/new_test.csv"
+    features = "examples/mi/data/WalmartSalesForecast/features.csv"
+    stores = "examples/mi/data/WalmartSalesForecast/stores.csv"
+
+    prompt = f"""
+    # Aim
+    Make predictions for Dept sales of the stores as accurate as possible.
+
+    # Datasets Available
+    - train_data: {train_data}
+    - test_data: {test_data}
+    - additional data: {features}, merge on train, test data.
+    - stores data: {stores}, merge on train, test data.
+
+    # Metric
+    The metric of the competition is weighted mean absolute error (WMAE).
+
+    # Notice
+    - *print* key variables to get more information for next task step.
+    - Make sure the DataFrame.dtypes can be accepted by model, such as lgbm, so data dtypes must be int, float or bool, and drop date column.
+    """
+    mi = Interpreter(use_tools=True)
+
+    await mi.run(prompt)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
