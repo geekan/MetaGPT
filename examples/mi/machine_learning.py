6652aa09ce8e2e19ba4a8ffd89013fae2fccb23f
刘棒棒 <liubangbang@fuzhi.ai>
2024-02-21 22:52:18 +0800

    delete feature.csv, store.csv, test.csv.

--- examples/mi/machine_learning.py
+++ examples/mi/machine_learning.py
@@ -10,22 +10,17 @@ DATA_DIR = "examples/mi/data/WalmartSalesForecast2"
 SALES_FORECAST_REQ = f"""
 # Goal
 Train a model to predict sales for each department in every store (split the last 40 weeks records as validation dataset,
-the others is train dataset), include plot sales trends, holiday effects, distribution of sales across stores/departments,
-using box on the train dataset, print metric and plot scatter plots of groud truth and predictions on validation data.
-save predictions on test data.
+the others is train dataset), include plot sales trends, print metric and plot scatter plots of
+groud truth and predictions on validation data.
 
 # Datasets Available
 - train_data: {DATA_DIR}/train.csv
-- test_data: {DATA_DIR}/test.csv, no label data.
-- additional data: {DATA_DIR}/features.csv
-- stores data: {DATA_DIR}/stores.csv
 
 # Metric
 The metric of the competition is weighted mean absolute error (WMAE) for test data.
 
 # Notice
 - *print* key variables to get more information for next task step.
-- Only When you fit the model, make the DataFrame.dtypes to be int, float or bool, and drop date column.
 """
 
 requirements = {"wine": WINE_REQ, "sales_forecast": SALES_FORECAST_REQ}
@@ -36,6 +31,7 @@ async def main(auto_run: bool = True, use_case: str = "wine"):
     if use_case == "wine":
         requirement = requirements[use_case]
     else:
+        mi.use_tools = True
         assert DATA_DIR != "your/path/to/data", f"Please set DATA_DIR for the use_case: {use_case}!"
         requirement = requirements[use_case]
     await mi.run(requirement)
