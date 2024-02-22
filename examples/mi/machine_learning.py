041a2d61097bad45b5b7e28a880b37268a285c5a
刘棒棒 <liubangbang@fuzhi.ai>
2024-02-22 21:56:02 +0800

    chore.

--- examples/mi/machine_learning.py
+++ examples/mi/machine_learning.py
@@ -4,7 +4,7 @@ from metagpt.roles.mi.interpreter import Interpreter
 
 WINE_REQ = "Run data analysis on sklearn Wine recognition dataset, include a plot, and train a model to predict wine class (20% as validation), and show validation accuracy."
 
-DATA_DIR = "examples/mi/data/WalmartSalesForecast2"
+DATA_DIR = "path/to/your/data"
 # sales_forecast data from https://www.kaggle.com/datasets/aslanahmedov/walmart-sales-forecast/data
 SALES_FORECAST_REQ = f"""Train a model to predict sales for each department in every store (split the last 40 weeks records as validation dataset, the others is train dataset), include plot total sales trends, print metric and plot scatter plots of
 groud truth and predictions on validation data. Dataset is {DATA_DIR}/train.csv, the metric is weighted mean absolute error (WMAE) for test data. Notice: *print* key variables to get more information for next task step.
