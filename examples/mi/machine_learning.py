08e00b4dc2894dc233bbb2802009f9f070092b3c
刘棒棒 <liubangbang@fuzhi.ai>
2024-02-22 21:48:05 +0800

    chore: simplify code.

--- examples/mi/machine_learning.py
+++ examples/mi/machine_learning.py
@@ -4,10 +4,10 @@ from metagpt.roles.mi.interpreter import Interpreter
 
 WINE_REQ = "Run data analysis on sklearn Wine recognition dataset, include a plot, and train a model to predict wine class (20% as validation), and show validation accuracy."
 
-DATA_DIR = "your/path/to/data"
+DATA_DIR = "examples/mi/data/WalmartSalesForecast2"
 # sales_forecast data from https://www.kaggle.com/datasets/aslanahmedov/walmart-sales-forecast/data
-SALES_FORECAST_REQ = f"""Train a model to predict sales for each department in every store (split the last 40 weeks records as validation dataset, the others is train dataset), include plot sales trends, print metric and plot scatter plots of
-groud truth and predictions on validation data. Datasets Available is train_data: {DATA_DIR}/train.csv, The metric of the competition is weighted mean absolute error (WMAE) for test data. Notice: *print* key variables to get more information for next task step.
+SALES_FORECAST_REQ = f"""Train a model to predict sales for each department in every store (split the last 40 weeks records as validation dataset, the others is train dataset), include plot total sales trends, print metric and plot scatter plots of
+groud truth and predictions on validation data. Dataset is {DATA_DIR}/train.csv, the metric is weighted mean absolute error (WMAE) for test data. Notice: *print* key variables to get more information for next task step.
 """
 
 REQUIREMENTS = {"wine": WINE_REQ, "sales_forecast": SALES_FORECAST_REQ}
@@ -15,12 +15,7 @@ REQUIREMENTS = {"wine": WINE_REQ, "sales_forecast": SALES_FORECAST_REQ}
 
 async def main(auto_run: bool = True, use_case: str = "wine"):
     mi = Interpreter(auto_run=auto_run)
-    if use_case == "wine":
-        requirement = REQUIREMENTS[use_case]
-    else:
-        mi.use_tools = True
-        assert DATA_DIR != "your/path/to/data", f"Please set DATA_DIR for the use_case: {use_case}!"
-        requirement = REQUIREMENTS[use_case]
+    requirement = REQUIREMENTS[use_case]
     await mi.run(requirement)
 
 
