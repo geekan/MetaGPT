2c858f8cbf2a0f5f7efcce0cd02a5ec9776868bc
wubinhao <15754305168@163.com>
2024-03-04 17:46:28 +0800

    update math expert demo

--- examples/mi/math_expert_demo.py
+++ examples/mi/math_expert_demo.py
@@ -1,24 +1,20 @@
 import fire
 from metagpt.roles.mi.math_expert import MathExpert
 
-# C.prob 容易
+# C.prob 容易  answer: 3
 p1 = """Roslyn has ten boxes. 
 Five of the boxes contain pencils, four of the boxes contain pens, and two of the boxes contain both pens and pencils. 
 How many boxes contain neither pens nor pencils?"""
 
-# C.prob 难
+# C.prob 难  answer: \frac{1}{12}
 p2 = """Three points are chosen randomly and independently on a circle. 
 What is the probability that all three pairwise distances between the points are less than the radius of the circle?"""
 
-# C.prob 简单
+# C.prob 简单  answer: 80
 p3 = """How many positive integers less than or equal to 100 have a prime factor that is greater than 4?"""
 
-# C.prob 难
-p4 = """Three points are chosen randomly and independently on a circle. 
-What is the probability that all three pairwise distances between the points are less than the radius of the circle?"""
-
-# C.prob 难
-p5 = """How many ways are there for 8 people to sit around a circular table if none of Alice, 
+# C.prob 难  answer: 1440
+p4 = """How many ways are there for 8 people to sit around a circular table if none of Alice, 
 Bob, and Eve (three of the 8 people) want to sit next to each other? 
 Two seatings are considered the same if one is a rotation of the other."""
 
