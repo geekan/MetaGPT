6ffa22ea335196864aaf57ebbaf32a20519d41bf
Abhishek0075 <abhirajp075@gmail.com>
2024-03-06 10:50:31 +0530

    black reformatting done

--- examples/search_with_specific_engine.py
+++ examples/search_with_specific_engine.py
@@ -8,24 +8,34 @@ from metagpt.roles import Searcher
 from metagpt.tools.search_engine import SearchEngine, SearchEngineType
 from metagpt.config2 import Config
 
+
 async def main():
     question = "What are the most interesting human facts?"
-    
+
     search = Config.default().search
     kwargs = {"api_key": search.api_key, "cse_id": search.cse_id, "proxy": None}
-    
-    if(search.api_type == SearchEngineType.DIRECT_GOOGLE):
+
+    if search.api_type == SearchEngineType.DIRECT_GOOGLE:
         # Google API
-        await Searcher(search_engine=SearchEngine(engine=SearchEngineType.DIRECT_GOOGLE, **kwargs)).run(question)
-    elif(search.api_type == SearchEngineType.SERPER_GOOGLE):
+        await Searcher(
+            search_engine=SearchEngine(engine=SearchEngineType.DIRECT_GOOGLE, **kwargs)
+        ).run(question)
+    elif search.api_type == SearchEngineType.SERPER_GOOGLE:
         # Serper API
-        await Searcher(search_engine=SearchEngine(engine=SearchEngineType.SERPER_GOOGLE, **kwargs)).run(question)
-    elif(search.api_type == SearchEngineType.SERPAPI_GOOGLE):
+        await Searcher(
+            search_engine=SearchEngine(engine=SearchEngineType.SERPER_GOOGLE, **kwargs)
+        ).run(question)
+    elif search.api_type == SearchEngineType.SERPAPI_GOOGLE:
         # SerpAPI
-        await Searcher(search_engine=SearchEngine(engine=SearchEngineType.SERPAPI_GOOGLE, **kwargs)).run(question)
+        await Searcher(
+            search_engine=SearchEngine(engine=SearchEngineType.SERPAPI_GOOGLE, **kwargs)
+        ).run(question)
     else:
         # DDG API
-        await Searcher(search_engine=SearchEngine(engine=SearchEngineType.DUCK_DUCK_GO, **kwargs)).run(question)
-    
+        await Searcher(
+            search_engine=SearchEngine(engine=SearchEngineType.DUCK_DUCK_GO, **kwargs)
+        ).run(question)
+
+
 if __name__ == "__main__":
     asyncio.run(main())
