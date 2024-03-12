a585ef52a60b3e2f4dfff19d512d9835cddf377e
yzlin <yzlin@fuzhi.ai>
2024-03-12 17:50:10 +0800

    update examples

--- examples/di/imitate_webpage.py
+++ examples/di/imitate_webpage.py
@@ -12,8 +12,7 @@ async def main():
     web_url = "https://pytorch.org/"
     prompt = f"""This is a URL of webpage: '{web_url}' .
 Firstly, utilize Selenium and WebDriver for rendering. 
-Secondly, convert image to a webpage including HTML, CSS and JS in one go. 
-Finally, save webpage in a text file. 
+Secondly, convert image to a webpage including HTML, CSS and JS in one go.
 Note: All required dependencies and environments have been fully installed and configured."""
     di = DataInterpreter(tools=["GPTvGenerator"])
 
