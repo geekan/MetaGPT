6a8699cd4a8f0f2f6f3a8095c67bc72abe775b35
莘权 马 <mashenquan@fuzhi.ai>
2024-03-12 11:14:35 +0800

    refactor: Replace Tree class with tree()

--- metagpt/utils/tree.py
+++ metagpt/utils/tree.py
@@ -5,61 +5,18 @@
 @Author  : mashenquan
 @File    : tree.py
 @Desc    : Implement the same functionality as the `tree` command.
-Example:
-        Usage:
+        Example:
             >>> print_tree(".")
             utils
             +-- serialize.py
             +-- project_repo.py
             +-- tree.py
             +-- mmdc_playwright.py
-            +-- dependency_file.py
-            +-- index.html
-            +-- make_sk_kernel.py
-            +-- token_counter.py
-            +-- embedding.py
-            +-- repair_llm_raw_output.py
-            +-- mermaid.py
-            +-- parse_html.py
-            +-- visual_graph_repo.py
-            +-- special_tokens.py
-            +-- ahttp_client.py
-            +-- __init__.py
-            +-- mmdc_ink.py
-            +-- di_graph_repository.py
-            +-- yaml_model.py
             +-- cost_manager.py
             +-- __pycache__
             |   +-- __init__.cpython-39.pyc
             |   +-- redis.cpython-39.pyc
             |   +-- singleton.cpython-39.pyc
-            |   +-- mmdc_ink.cpython-39.pyc
-            |   +-- read_document.cpython-39.pyc
-            |   +-- mermaid.cpython-39.pyc
-            |   +-- parse_html.cpython-39.pyc
-            |   +-- human_interaction.cpython-39.pyc
-            |   +-- cost_manager.cpython-39.pyc
-            |   +-- json_to_markdown.cpython-39.pyc
-            |   +-- graph_repository.cpython-39.pyc
-            |   +-- ahttp_client.cpython-39.pyc
-            |   +-- visual_graph_repo.cpython-39.pyc
-            |   +-- file.cpython-39.pyc
-            |   +-- di_graph_repository.cpython-39.pyc
-            |   +-- pycst.cpython-39.pyc
-            |   +-- save_code.cpython-39.pyc
-            |   +-- dependency_file.cpython-39.pyc
-            |   +-- text.cpython-39.pyc
-            |   +-- token_counter.cpython-39.pyc
-            |   +-- project_repo.cpython-39.pyc
-            |   +-- yaml_model.cpython-39.pyc
-            |   +-- serialize.cpython-39.pyc
-            |   +-- git_repository.cpython-39.pyc
-            |   +-- custom_decoder.cpython-39.pyc
-            |   +-- parse_docstring.cpython-39.pyc
-            |   +-- common.cpython-39.pyc
-            |   +-- exceptions.cpython-39.pyc
-            |   +-- repair_llm_raw_output.cpython-39.pyc
-            |   +-- s3.cpython-39.pyc
             |   +-- embedding.cpython-39.pyc
             |   +-- make_sk_kernel.cpython-39.pyc
             |   +-- file_repository.cpython-39.pyc
@@ -67,22 +24,6 @@ Example:
             +-- save_code.py
             +-- common.py
             +-- redis.py
-            +-- text.py
-            +-- graph_repository.py
-            +-- singleton.py
-            +-- recovery_util.py
-            +-- file_repository.py
-            +-- pycst.py
-            +-- exceptions.py
-            +-- human_interaction.py
-            +-- highlight.py
-            +-- mmdc_pyppeteer.py
-            +-- s3.py
-            +-- json_to_markdown.py
-            +-- custom_decoder.py
-            +-- git_repository.py
-            +-- read_document.py
-            +-- parse_docstring.py
 """
 from __future__ import annotations
 
