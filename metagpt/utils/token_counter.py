c9f6a52752c12adc8e6c4666abac8036c1b5d9f2
Ruifeng Fu <ruifeng@tju.edu.cn>
2024-03-17 21:53:10 +0800

    update gpt-3.5-turbo-0125 in TOKEN_COSTS

--- metagpt/utils/token_counter.py
+++ metagpt/utils/token_counter.py
@@ -21,6 +21,7 @@ TOKEN_COSTS = {
     "gpt-35-turbo": {"prompt": 0.0015, "completion": 0.002},
     "gpt-35-turbo-16k": {"prompt": 0.003, "completion": 0.004},
     "gpt-3.5-turbo-1106": {"prompt": 0.001, "completion": 0.002},
+    "gpt-3.5-turbo-0125": {"prompt": 0.001, "completion": 0.002},
     "gpt-4-0314": {"prompt": 0.03, "completion": 0.06},
     "gpt-4": {"prompt": 0.03, "completion": 0.06},
     "gpt-4-32k": {"prompt": 0.06, "completion": 0.12},
