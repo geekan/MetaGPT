12e90095294f7118974ad3be65407ef39ef6745b
stellahsr <stellahsr@126.com>
2024-03-19 20:12:54 +0800

    rm useless comments

--- data/inference/run.py
+++ data/inference/run.py
@@ -4,16 +4,12 @@
 import runpy
 import sys
 
-# 备份原始的sys.argv
 original_argv = sys.argv.copy()
 
 try:
     # 设置你想要传递给脚本的命令行参数
     sys.argv = ['run_api.py', '--dataset_name_or_path', 'princeton-nlp/SWE-bench_oracle', '--output_dir',
                 './outputs']
-    # 添加其他可选参数到sys.argv中，例如:
-    # sys.argv.extend(['--some_option', 'some_value'])
-    
     # 执行脚本
     runpy.run_path(path_name='run_api.py', run_name='__main__')
 finally:
