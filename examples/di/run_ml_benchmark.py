bc0d7b0620071f30f22823ef9522da8eabe66c26
Your Name <you@example.com>
2024-03-20 22:05:31 +0800

    Update README.md and add tool_recommend

--- examples/di/run_ml_benchmark.py
+++ examples/di/run_ml_benchmark.py
@@ -5,6 +5,7 @@ import fire
 from examples.di.requirements_prompt import ML_BENCHMARK_REQUIREMENTS
 from metagpt.const import DATA_PATH
 from metagpt.roles.di.data_interpreter import DataInterpreter
+from metagpt.tools.tool_recommend import TypeMatchToolRecommender
 
 
 # Ensure ML-Benchmark dataset has been downloaded before using these example.
@@ -13,7 +14,7 @@ async def main(task_name, data_dir=DATA_PATH, use_reflection=True):
         raise FileNotFoundError(f"ML-Benchmark dataset not found in {data_dir}.")
 
     requirement = ML_BENCHMARK_REQUIREMENTS[task_name].format(data_dir=data_dir)
-    di = DataInterpreter(use_reflection=use_reflection)
+    di = DataInterpreter(use_reflection=use_reflection, tool_recommender=TypeMatchToolRecommender(tools=["<all>"]))
     await di.run(requirement)
 
 
--- examples/di/run_open_ended_tasks.py
+++ examples/di/run_open_ended_tasks.py
@@ -5,6 +5,7 @@ import fire
 from examples.di.requirements_prompt import OPEN_ENDED_TASKS_REQUIREMENTS
 from metagpt.const import DATA_PATH
 from metagpt.roles.di.data_interpreter import DataInterpreter
+from metagpt.tools.tool_recommend import TypeMatchToolRecommender
 
 
 # Ensure Open-Ended Tasks dataset has been downloaded before using this example.
@@ -13,7 +14,7 @@ async def main(task_name, data_dir=DATA_PATH, use_reflection=True):
         raise FileNotFoundError(f"Open-ended task dataset not found in {data_dir}.")
 
     requirement = OPEN_ENDED_TASKS_REQUIREMENTS[task_name].format(data_dir=data_dir)
-    di = DataInterpreter(use_reflection=use_reflection)
+    di = DataInterpreter(use_reflection=use_reflection, tool_recommender=TypeMatchToolRecommender(tools=["<all>"]))
     await di.run(requirement)
 
 
