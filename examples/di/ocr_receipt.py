587c13489320c62c8958f57bea54842e5a993614
莘权 马 <mashenquan@fuzhi.ai>
2024-03-21 14:55:00 +0800

    fixbug: reset ocr_receipt.py

--- examples/di/ocr_receipt.py
+++ examples/di/ocr_receipt.py
@@ -8,7 +8,7 @@ async def main():
     requirement = f"""This is a {language} receipt image.
     Your goal is to perform OCR on images using PaddleOCR, output text content from the OCR results and discard 
     coordinates and confidence levels, then recognize the total amount from ocr text content, and finally save as table. 
-    Image path: {image_path}. 
+    Image path: {image_path}.
     NOTE: The environments for Paddle and PaddleOCR are all ready and has been fully installed."""
     di = DataInterpreter()
 
