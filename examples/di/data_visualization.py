82ca12f4da251ae2b63d9723eea26c98f5b87b4d
luxiangtao <luxiangtao@fuzhi.ai>
2024-03-25 11:24:02 +0800

    pre-commit

--- examples/di/data_visualization.py
+++ examples/di/data_visualization.py
@@ -1,10 +1,11 @@
 import asyncio
+
 from metagpt.logs import logger
-from metagpt.utils.recovery_util import save_history
 from metagpt.roles.di.data_interpreter import DataInterpreter
+from metagpt.utils.recovery_util import save_history
+
 
 async def main(requirement: str = ""):
-
     di = DataInterpreter()
     rsp = await di.run(requirement)
     logger.info(rsp)
@@ -12,7 +13,5 @@ async def main(requirement: str = ""):
 
 
 if __name__ == "__main__":
-
     requirement = "Run data analysis on sklearn Iris dataset, include a plot"
     asyncio.run(main(requirement))
-
