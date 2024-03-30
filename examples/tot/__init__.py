42c89de4db9dd17a01a7a99d2d17e608f55baa93
stellahsr <stellahsr@126.com>
2024-03-30 14:05:58 +0800

    rm tot/

--- examples/tot/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Date    : 1/12/2024 6:05 PM
-# @Author  : stellahong (stellahong@fuzhi.ai)
-# @Desc    :
--- examples/tot/prompt_templates/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Date    : 12/23/2023 5:21 PM
-# @Author  : stellahong (stellahong@fuzhi.ai)
-# @Desc    :
