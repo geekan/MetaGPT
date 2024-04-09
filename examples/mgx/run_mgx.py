3e83a8d7fa77df187361b301c942f8b0a123710a
stellahsr <stellahsr@126.com>
2024-04-09 16:35:59 +0800

    update example

--- examples/mgx/run_mgx.py
+++ examples/mgx/run_mgx.py
@@ -9,12 +9,13 @@ requirement = (
     # "design a game using Gym (an open source Python library), including a graphical interface and interactive gameplay"
     # "帮我把pip的源设置成：https://pypi.tuna.tsinghua.edu.cn/simple"
     # "This is a website url does not require login: https://demosc.chinaz.net/Files/DownLoad//moban/202404/moban7767 please write a similar web page,developed in vue language, The package.json dependency must be generated"
-    "I would like to imitate the website available at https://demosc.chinaz.net/Files/DownLoad//moban/202404/moban7767. Could you please browse through it?"
+    # "I would like to imitate the website available at https://demosc.chinaz.net/Files/DownLoad//moban/202404/moban7767. Could you please browse through it?"
+    "Create a 2048 Game"
 )
 
 
 async def main(requirement: str = ""):
-    mgx = MGX(use_intent=True)
+    mgx = MGX(use_intent=True, tools=["software development"])
     await mgx.run(requirement)
 
 
