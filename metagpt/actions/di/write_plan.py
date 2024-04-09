f8f706fac6340b0e2ca4fe6c3863f5e78a4ab9d3
莘权 马 <mashenquan@fuzhi.ai>
2024-04-09 17:12:06 +0800

    feat: revert task instruction

--- metagpt/actions/di/write_plan.py
+++ metagpt/actions/di/write_plan.py
@@ -33,8 +33,8 @@ Output a list of jsons following the format:
     {{
         "task_id": str = "unique identifier for a task in plan, can be an ordinal",
         "dependent_task_ids": list[str] = "ids of tasks prerequisite to this task",
-        "instruction": "what you should do in this task, one short phrase or sentence. If the SOP description is provided in the `Knowledge` section, the reference to the SOP description should be included intact in the instruction.",
-        "task_type": "type of this task, should be one of Available Task Types. You can refer to the hints in the `SOP Type` section to make a selection if `SOP Type` section is provided.",
+        "instruction": "what you should do in this task, one short phrase or sentence.",
+        "task_type": "type of this task, should be one of Available Task Types.",
     }},
     ...
 ]
