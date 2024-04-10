8d78632dd084e8fb4c078f9368e38f09069249fa
stellahsr <stellahsr@126.com>
2024-04-10 16:33:38 +0800

    update local code

--- examples/di/rm_image_background.py
+++ examples/di/rm_image_background.py
@@ -1,15 +1,16 @@
 import asyncio
 
-from metagpt.roles.di.data_interpreter import DataInterpreter
+from metagpt.roles.di.mgx import MGX
 
 
 async def main(requirement: str = ""):
-    di = DataInterpreter()
+    # di = DataInterpreter()
+    di = MGX(use_intent=False, tools=["<all>"])
     await di.run(requirement)
 
 
 if __name__ == "__main__":
-    image_path = "/your/path/to/the/image.jpeg"
-    save_path = "/your/intended/save/path/for/image_rm_bg.png"
+    image_path = r"F:\deepWisdom\metaGPT\hsr\MetaGPT\examples\data\dog.beebf16d.jpg"
+    save_path = r"F:\deepWisdom\metaGPT\hsr\MetaGPT\examples\data\/image_rm_bg.png"
     requirement = f"This is a image, you need to use python toolkit rembg to remove the background of the image and save the result. image path:{image_path}; save path:{save_path}."
     asyncio.run(main(requirement))
