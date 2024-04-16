211ba3dce11923fb536b358c0df3b312df994a12
YangQianli92 <108046369+YangQianli92@users.noreply.github.com>
2024-04-16 11:32:40 +0800

    Add files via upload

--- metagpt/rag/factories/ranker.py
+++ metagpt/rag/factories/ranker.py
@@ -3,9 +3,7 @@
 from llama_index.core.llms import LLM
 from llama_index.core.postprocessor import LLMRerank
 from llama_index.core.postprocessor.types import BaseNodePostprocessor
-from llama_index.postprocessor.colbert_rerank import ColbertRerank
-from llama_index.postprocessor.cohere_rerank import CohereRerank
-from llama_index.postprocessor.flag_embedding_reranker import FlagEmbeddingReranker
+
 from metagpt.rag.factories.base import ConfigBasedFactory
 from metagpt.rag.rankers.object_ranker import ObjectSortPostprocessor
 from metagpt.rag.schema import (
@@ -55,11 +53,22 @@ class RankerFactory(ConfigBasedFactory):
         return ObjectSortPostprocessor(**config.model_dump())
 
     def _create_cohere_rerank(self, config: CohereRerankConfig, **kwargs) -> LLMRerank:
+        try:
+            from llama_index.postprocessor.cohere_rerank import CohereRerank
+        except ImportError:
+            raise ImportError(
+                "`llama-index-postprocessor-cohere-rerank` package not found, please run `pip install llama-index-postprocessor-cohere-rerank`"
+            )
         return CohereRerank(**config.model_dump())
 
-    def _create_flag_rerank(self, config: FlagEmbeddingReranker, **kwargs) -> LLMRerank:
+    def _create_flag_rerank(self, config: FlagEmbeddingConfig, **kwargs) -> LLMRerank:
+        try:
+            from llama_index.postprocessor.flag_embedding_reranker import FlagEmbeddingReranker
+        except ImportError:
+            raise ImportError(
+                "`llama-index-postprocessor-flag-embedding-reranker` package not found, please run `pip install llama-index-postprocessor-flag-embedding-reranker`"
+            )
         return FlagEmbeddingReranker(**config.model_dump())
-
 
     def _extract_llm(self, config: BaseRankerConfig = None, **kwargs) -> LLM:
         return self._val_from_config_or_kwargs("llm", config, **kwargs)
