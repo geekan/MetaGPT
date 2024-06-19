30286ce4b86fbf02a8178ff82ac9a73986cc4e1c
garylin2099 <yzlin@fuzhi.ai>
2024-06-19 16:04:55 +0800

    fix da not responded to new req; rm user_req attribute from rolezero

--- metagpt/roles/di/role_zero.py
+++ metagpt/roles/di/role_zero.py
@@ -53,7 +53,6 @@ class RoleZero(Role):
     experience_retriever: ExpRetriever = DummyExpRetriever()
 
     # Others
-    user_requirement: str = ""
     command_rsp: str = ""  # the raw string containing the commands
     commands: list[dict] = []  # commands to be executed
     memory_k: int = 20  # number of memories (messages) to use as historical context
@@ -106,8 +105,7 @@ class RoleZero(Role):
             return False
 
         if not self.planner.plan.goal:
-            self.user_requirement = self.get_memories()[-1].content
-            self.planner.plan.goal = self.user_requirement
+            self.planner.plan.goal = self.get_memories()[-1].content
 
         ### 1. Experience ###
         example = self._retrieve_experience()
