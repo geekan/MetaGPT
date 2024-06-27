742ff0e80a95a3cea87b8e8e442a68aa060d9ca9
莘权 马 <mashenquan@fuzhi.ai>
2024-06-27 14:35:05 +0800

    feat: +pic2txt

---
+++ tests/metagpt/actions/requirement_analysis/requirement/test_pic2txt.py
@@ -0,0 +1,28 @@
+import pytest
+
+from metagpt.actions.requirement_analysis.requirement.pic2txt import Pic2Txt
+from metagpt.const import TEST_DATA_PATH
+from metagpt.utils.common import aread
+
+
+@pytest.mark.asyncio
+async def test_pic2txt(context):
+    images = [
+        TEST_DATA_PATH / "requirements/pic/1.png",
+        TEST_DATA_PATH / "requirements/pic/2.png",
+        TEST_DATA_PATH / "requirements/pic/3.png",
+        TEST_DATA_PATH / "requirements/pic/4.png",
+        TEST_DATA_PATH / "requirements/pic/5.png",
+    ]
+    textual_user_requirements = await aread(filename=TEST_DATA_PATH / "requirements/1.original_requirement.txt")
+    acknowledge = await aread(filename=TEST_DATA_PATH / "requirements/1.acknowledge.md")
+
+    action = Pic2Txt(context=context)
+    rsp = await action.run(
+        image_paths=images, textual_user_requirement=textual_user_requirements, acknowledge=acknowledge
+    )
+    assert rsp
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-s"])
