24332620b7cb44b9420cf20474186a06251c0690
lidanyang <lidanyang@fuzhi.ai>
2024-07-22 15:26:55 +0800

    define assignee to role name

--- metagpt/schema.py
+++ metagpt/schema.py
@@ -670,7 +670,7 @@ class Plan(BaseModel):
         return [task for task in self.tasks if task.is_finished]
 
     def append_task(self, task_id: str, dependent_task_ids: list[str], instruction: str, assignee: str, task_type: str = ""):
-        """Append a new task with task_id (number) to the end of existing task sequences. If dependent_task_ids is not empty, the task will depend on the tasks with the ids in the list. Note that the assignee is the role name after word `named`."""
+        """Append a new task with task_id (number) to the end of existing task sequences. If dependent_task_ids is not empty, the task will depend on the tasks with the ids in the list. Note that the assignee should be the 'name' of the role."""
         new_task = Task(
             task_id=task_id,
             dependent_task_ids=dependent_task_ids,
