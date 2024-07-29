f31c257ea10e660823c5dd06f2813761317611e3
garylin2099 <yzlin@fuzhi.ai>
2024-07-29 21:29:11 +0800

    add search kw to hit example

--- metagpt/strategy/experience_retriever.py
+++ metagpt/strategy/experience_retriever.py
@@ -663,7 +663,7 @@ class KeywordExpRetriever(ExpRetriever):
                 return DEPLOY_EXAMPLE
             elif "issue" in context.lower():
                 return FIX_ISSUE_EXAMPLE
-            elif "https:" in context.lower() or "http:" in context.lower():
+            elif "https:" in context.lower() or "http:" in context.lower() or "search" in context.lower():
                 if "search" in context.lower() or "click" in context.lower():
                     return WEB_SCRAPING_EXAMPLE
                 return WEB_SCRAPING_EXAMPLE_SIMPLE
