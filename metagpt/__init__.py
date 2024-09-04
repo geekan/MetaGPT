2415cbce001122dafd1ca21a156531a160dd9a11
Abir HARRASSE <81148161+abirharrasse@users.noreply.github.com>
2024-09-04 23:34:33 +0100

    Update __init__.py

--- metagpt/__init__.py
+++ metagpt/__init__.py
@@ -5,3 +5,34 @@
 # @File    : __init__.py
 
 from metagpt import _compat as _  # noqa: F401
+from metagpt.provider.google_gemini_api import GeminiLLM
+from metagpt.provider.ollama_api import OllamaLLM
+from metagpt.provider.openai_api import OpenAILLM
+from metagpt.provider.zhipuai_api import ZhiPuAILLM
+from metagpt.provider.azure_openai_api import AzureOpenAILLM
+from metagpt.provider.metagpt_api import MetaGPTLLM
+from metagpt.provider.human_provider import HumanProvider
+from metagpt.provider.spark_api import SparkLLM
+from metagpt.provider.qianfan_api import QianFanLLM
+from metagpt.provider.dashscope_api import DashScopeLLM
+from metagpt.provider.anthropic_api import AnthropicLLM
+from metagpt.provider.bedrock_api import BedrockLLM
+from metagpt.provider.ark_api import ArkLLM
+
+__all__ = [
+    "GeminiLLM",
+    "OpenAILLM",
+    "ZhiPuAILLM",
+    "AzureOpenAILLM",
+    "MetaGPTLLM",
+    "OllamaLLM",
+    "HumanProvider",
+    "SparkLLM",
+    "QianFanLLM",
+    "DashScopeLLM",
+    "AnthropicLLM",
+    "BedrockLLM",
+    "ArkLLM",
+    "OwnLLM",
+    "TogetherLLM"
+]
