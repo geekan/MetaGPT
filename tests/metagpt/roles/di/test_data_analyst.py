00cdbbb85cbf620ef340b12faee2d61265df9072
莘权 马 <mashenquan@fuzhi.ai>
2024-09-12 13:38:05 +0800

    feat: +similarity_search

--- tests/metagpt/roles/di/test_data_analyst.py
+++ tests/metagpt/roles/di/test_data_analyst.py
@@ -4,6 +4,7 @@ from metagpt.const import TEST_DATA_PATH
 from metagpt.roles.di.data_analyst import DataAnalyst
 
 
+@pytest.mark.skip
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     ("query", "filename"), [("similarity search about '有哪些需求描述？' in document ", TEST_DATA_PATH / "requirements/2.pdf")]
