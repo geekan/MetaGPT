e3bcedc2984006714151c73fd9fd247b58663436
didi <84363704+didiforgithub@users.noreply.github.com>
2024-09-22 15:54:32 +0800

    Update Benchmark's data

--- examples/ags/benchmark/utils.py
+++ examples/ags/benchmark/utils.py
@@ -1,3 +1,4 @@
+import json
 import numpy as np
 
 def generate_random_indices(n, n_samples, test=False):
@@ -15,5 +16,19 @@ def generate_random_indices(n, n_samples, test=False):
         return indices[n_samples:]
     else:
         return indices[:n_samples]
-    
-# TODO yzy 补充分割数据集的函数
+
+def split_data_set(file_path, samples, test=False):
+    data = []
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            data.append(json.loads(line))
+    random_indices = generate_random_indices(len(data), samples, test)
+    data = [data[i] for i in random_indices]
+    return data
+
+# save data into a jsonl file
+def save_data(data, file_path):
+    with open(file_path, 'w') as file:
+        for d in data:
+            file.write(json.dumps(d) + '\n')
