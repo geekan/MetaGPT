7c0adf502593668c954c79b22bea9da55fe24421
seehi <6580@pm.me>
2024-09-29 11:27:39 +0800

    make the longterm-memory path configurable

--- metagpt/configs/role_zero_config.py
+++ metagpt/configs/role_zero_config.py
@@ -5,3 +5,4 @@ from metagpt.utils.yaml_model import YamlModel
 
 class RoleZeroConfig(YamlModel):
     enable_longterm_memory: bool = Field(default=False, description="Whether to use long-term memory.")
+    longterm_memory_persist_path: str = Field(default=".role_memory_data", description="The directory to save data.")
