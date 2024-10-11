9c54113f777990174bacd0ea789435f53982e71a
Yizhou Chi <chiyizhou@fuzhi.ai>
2024-10-11 10:41:33 +0800

    make sure image task starting from datapreprocessing

--- expo/run_experiment.py
+++ expo/run_experiment.py
@@ -31,6 +31,7 @@ def get_mcts_args(parser):
     parser.set_defaults(load_tree=False)
     parser.add_argument("--rollouts", type=int, default=5)
     parser.add_argument("--use_fixed_insights", dest="use_fixed_insights", action="store_true")
+    parser.add_argument("--start_task_id", type=int, default=2)
 
 
 def get_aug_exp_args(parser):
