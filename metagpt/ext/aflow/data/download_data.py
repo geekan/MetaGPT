bbb982468c52ded178848463f74765da28b15497
didi <84363704+didiforgithub@users.noreply.github.com>
2024-10-24 20:09:37 +0800

    Update download_data.py

--- metagpt/ext/aflow/data/download_data.py
+++ metagpt/ext/aflow/data/download_data.py
@@ -55,7 +55,11 @@ datasets_to_download: Dict[str, Dict[str, str]] = {
         "filename": "aflow_data.tar.gz",
         "extract_path": "metagpt/ext/aflow/data",
     },
-    "results": {"url": "", "filename": "result.tar.gz", "extract_path": "metagpt/ext/aflow/data/results"},
+    "results": {
+        "url": "https://drive.google.com/uc?export=download&id=1Sr5wjgKf3bN8OC7G6cO3ynzJqD4w6_Dv",
+        "filename": "result.tar.gz",
+        "extract_path": "metagpt/ext/aflow/data/results",
+    },
     "initial_rounds": {
         "url": "https://drive.google.com/uc?export=download&id=1UBoW4WBWjX2gs4I_jq3ALdXeLdwDJMdP",
         "filename": "initial_rounds.tar.gz",
