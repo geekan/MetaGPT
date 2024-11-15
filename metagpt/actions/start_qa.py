63561bbef2f9535a2c017ac65e0875c7ccee481f
莘权 马 <mashenquan@fuzhi.ai>
2024-11-15 18:05:45 +0800

    refactor: remove obsolete codes

---
+++ metagpt/actions/start_qa.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2024/11/15
+@Author  : mashenquan
+@File    : start_qa.py
+"""
+from metagpt.actions import Action
+
+
+class StartQA(Action):
+    """A notification event to QAEngineer without any implementation details"""
